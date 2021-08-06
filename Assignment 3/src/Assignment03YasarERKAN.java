7730526a544cff5aee98abecea5e5e492f56c748
Yasar ERKAN <ysr.erk2019@gmail.com>
2021-08-06 10:55:37 -0400

    Added programmer comments, and starter code to method main.

--- Assignment 3/src/Assignment03YasarERKAN.java
+++ Assignment 3/src/Assignment03YasarERKAN.java
@@ -1,8 +1,35 @@
-
+// This class created for Assignment03 by YasarERKAN
 public class Assignment03YasarERKAN {
-
+	
+	// This is a main method 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+				// Defining and initializing variables named roll and totalRolls
+				int roll = 0;
+				int totalRolls = 0;
+				//An array definition that can hold 20 values of int type is being made here
+				int[] diceRolls = new int[20];
+				
+
+				// Roll values are creating by math.random method  and for loop.
+				//Also they are assigning to array  was named diceRolls 
+				for(int count = 0; count < 1000; count++) {
+				    roll = (int)(Math.random() * 20) + 1;
+				    diceRolls[roll] = diceRolls[roll - 1] + 1; // crashes
+				}	
+				// This for loop creating  report for all array values.
+				//Also  it calculates totalRolls value
+				for(int index = 0; index < diceRolls.length; index++) {
+				    System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
+				    totalRolls = totalRolls + diceRolls[index];
+				}
+				// These codes print to screen  total rolls and programmer name
+				System.out.println("Total rolls were: " + totalRolls);
+				System.out.println("Program by Yasar ERKAN");
+
 
 	}
 
